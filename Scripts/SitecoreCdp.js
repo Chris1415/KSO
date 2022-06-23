18ba7e523e9854da996d628ef874fbd6a74643ca
Christian <christian-hahn1@gmx.net>
2022-06-23 12:41:47 +0200

    Added the trick to inject an id where no id is ^^

--- Scripts/SitecoreCdp.js
+++ Scripts/SitecoreCdp.js
@@ -47,6 +47,14 @@ function FunctionsAfterLoad(){
 	getLocation();
 }
 
+ function DoTheTrick(){
+//*** CUSTOM TRICK
+	var targetElements = document.getElementsByClassName(""component container col-xs-12 full-width-promo"");
+	if(targetElements && targetElements[0]){
+    		targetElements[0].id = "PersonalizedHeroTeaser";
+	}
+}
+
 function AttachToRegistration(){
 	var currentUrl = window.location.href;
 	if(currentUrl.includes("register")){
